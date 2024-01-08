fad03a4f60e8a6f47124e021b7d6e05ba97e1007
Duyenltmce23 <ltmduyen2k3@gmail.com>
2024-01-08 21:16:08 +0700

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,7 @@
+console.log("hello");
+
+    const hamburger = document.querySelector('#hamburger');
+    hamburger.addEventListener('click', function () {
+        console.log('Click hamburger');
+    });
+
